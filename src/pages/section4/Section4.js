6ea2cb622504b3a65a32a2d98408c413b24547d1
Asilbekcoder14 <asilbekgiyosiddinov99@gmail.com>
2023-12-16 21:11:19 +0500

    blablabla

--- src/pages/section4/Section4.js
+++ src/pages/section4/Section4.js
@@ -1,9 +1,271 @@
-import React from 'react'
+import React from "react";
+import Style from "./Section4.module.css";
+import img from "./image/XAWO3cHvnE.png";
+import img1 from "./image/Group 84.png";
+import img2 from "./image/TMrjigyd3v.png";
+import img3 from "./image/5N8cjhxWSH 1.png";
+import img4 from "./image/sizf4VPAqn.png";
+import img5 from "./image/ba5inQ7lOC 1.png";
+import img6 from "./image/CcKMDGIS9J 1.png";
+import img7 from "./image/shot 1.png";
+import img8 from "./image/apple 1.svg";
+import img9 from "./image/play 1.svg";
+import img10 from "./image/rbUCqO2zGk 1.png";
+import img11 from "./image/Group 107.png";
+import img12 from "./image/Group 37.png";
 
 function Section4() {
   return (
-    <div>Section4</div>
-  )
+    <div className="App">
+      <div className={Style.menu}>
+        <div className={Style.menu2}>
+          <h2>Menu</h2>
+          <h1>Food Full of treaty Love</h1>
+          <p>
+            There are many things are needed to start the Fast Food Business.
+            You need not only Just Food Stalls <br /> with Persons but also
+            specialized equipment, Skills to manage Customers,{" "}
+          </p>
+        </div>
+
+        <div className={Style.card}>
+          <div className={Style.cardmini}>
+            <img src={img} alt="" />
+            <div className={Style.h2}>
+              <h2>Vegie Muffen</h2> <h2>16$</h2>
+            </div>
+            <p>
+              There are many things are needed to <br /> start the Fast Food
+              Business.
+            </p>
+            <div className={Style.box}>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M6.74868 6.74868V12H5.25132V6.74868H0V5.25132H5.25132V0H6.74868V5.25132H12V6.74868H6.74868Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+              <img src={img1} alt="" />
+            </div>
+          </div>
+          <div className={Style.cardmini}>
+            <img src={img2} alt="" />
+            <div className={Style.h2}>
+              <h2>Salads</h2> <h2>12$</h2>
+            </div>
+            <p>
+              There are many things are needed to <br /> start the Fast Food
+              Business.
+            </p>
+            <div className={Style.box}>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M6.74868 6.74868V12H5.25132V6.74868H0V5.25132H5.25132V0H6.74868V5.25132H12V6.74868H6.74868Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+              <img src={img1} alt="" />
+            </div>
+          </div>
+          <div className={Style.cardmini}>
+            <img src={img3} alt="" />
+            <div className={Style.h2}>
+              <h2>Burger</h2> <h2>10$</h2>
+            </div>
+            <p>
+              There are many things are needed to <br /> start the Fast Food
+              Business.
+            </p>
+            <div className={Style.box}>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M6.74868 6.74868V12H5.25132V6.74868H0V5.25132H5.25132V0H6.74868V5.25132H12V6.74868H6.74868Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+              <img src={img1} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className={Style.cardmini2}>
+          <div className={Style.cardmini}>
+            <img src={img5} alt="" />
+            <div className={Style.h2}>
+              <h2>Egg Masala</h2> <h2>9$</h2>
+            </div>
+            <p>
+              There are many things are needed to <br /> start the Fast Food
+              Business.
+            </p>
+            <div className={Style.box}>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M6.74868 6.74868V12H5.25132V6.74868H0V5.25132H5.25132V0H6.74868V5.25132H12V6.74868H6.74868Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+              <img src={img1} alt="" />
+            </div>
+          </div>
+          <div className={Style.cardmini}>
+            <img src={img4} alt="" />
+            <div className={Style.h2}>
+              <h2> Steak dish</h2> <h2>14$</h2>
+            </div>
+            <p>
+              There are many things are needed to <br /> start the Fast Food
+              Business.
+            </p>
+            <div className={Style.box}>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M6.74868 6.74868V12H5.25132V6.74868H0V5.25132H5.25132V0H6.74868V5.25132H12V6.74868H6.74868Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+              <img src={img1} alt="" />
+            </div>
+          </div>
+          <div className={Style.cardmini}>
+            <img src={img6} alt="" />
+            <div className={Style.h2}>
+              <h2>Peach Melba </h2> <h2>15$</h2>
+            </div>
+            <p>
+              There are many things are needed to <br /> start the Fast Food
+              Business.
+            </p>
+            <div className={Style.box}>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                >
+                  <path
+                    d="M6.74868 6.74868V12H5.25132V6.74868H0V5.25132H5.25132V0H6.74868V5.25132H12V6.74868H6.74868Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+              <img src={img1} alt="" />
+            </div>
+          </div>
+        </div>
+        <button>Learn More</button>
+      </div>
+      <div className={Style.take}>
+        <div className={Style.take1}>
+          <h2>Take away</h2>
+          <h1>
+            Food Stalls with Persons but to <br /> Product marketing plane.{" "}
+          </h1>
+          <p>
+            There are many things are needed to start the Fast Food Business.
+            You need <br /> not only Just Food Stalls with Persons but also
+            specialized equipmentwith <br /> Persons but also Just Food Stalls
+            with Persons.
+          </p>
+          <div className={Style.button}>
+            <button>
+              {" "}
+              <img src={img8} alt="" /> App Store
+            </button>
+            <button>
+              {" "}
+              <img src={img9} alt="" /> Google Play
+            </button>
+          </div>
+        </div>
+        <div className={Style.take2}>
+          <img src={img7} alt="" />
+        </div>
+      </div>
+
+      <div className={Style.make}>
+        <div className={Style.make1}>
+          <h2>Testimonial</h2>
+          <h1>Making Food great again and again</h1>
+          <p>
+            You need not only Just Food Stalls with Persons but also specialized
+            equipment, Skills to manage <br /> Customers, Effective Product
+            catlogues etc to make your.
+          </p>
+          <img src={img10} alt="" />
+        </div>
+        <div className={Style.make2}>
+          <div className={Style.make3}>
+            <img src={img11} alt="" />
+            <p>
+              You need not only Just Food Stalls with Persons but also
+              specialized <br /> equipment, Skills to manage Customers,
+              Effective Product <br /> etc very successful to make your.
+            </p>
+            <img src={img12} alt="" />
+          </div>
+          <div className={Style.make4}>
+            <img src={img1} alt="" />
+            <h2>Augusta W. Reynoso</h2>
+            <p>UI&UX DeSIGNER</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={Style.contact}>
+        <h2>Contact </h2>
+        <h1>
+          Food Stalls with Persons but also specialized <br /> equipment, Skills
+          to manage.
+        </h1>
+        <div className={Style.input}>
+          <input type="text" placeholder="Enter your message" />
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Section4
+export default Section4;
